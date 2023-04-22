9a378cca6acd2b07cde934bc0b42afa9f6f7b182
qwq <qwq@qwq.cat>
2023-04-22 21:52:14 +0800

    feat: add `deepmerge` options

--- src/utils/extend-config.ts
+++ src/utils/extend-config.ts
@@ -2,6 +2,6 @@ import config from '..'
 import deepmerge from 'deepmerge'
 import { type Config } from 'prettier'
 
-export const extendConfig = <T extends Config | typeof config>(userConfig: T) => {
-  return deepmerge(config, userConfig)
+export const extendConfig = <T extends Config | typeof config>(userConfig: T, options?: deepmerge.Options) => {
+  return deepmerge(config, userConfig, options)
 }
