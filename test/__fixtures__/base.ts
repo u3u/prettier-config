f33d3d945138c0bdb327ccf3b385eb3c740bf75b
qwq <qwq@qwq.cat>
2023-11-13 13:58:01 +0800

    test: update snapshot

--- test/__fixtures__/base.ts
+++ test/__fixtures__/base.ts
@@ -6,3 +6,14 @@ import type{Config} from 'prettier'
 const obj=/** @type {    const} */({
   name:"test"
 })
+
+const i = 0
+
+const message =
+  i % 3 === 0 && i % 5 === 0
+    ? "fizzbuzz"
+  : i % 3 === 0
+    ? "fizz"
+  : i % 5 === 0
+    ? "buzz"
+    : i % 6 === 0 ? "foo": String(i);
