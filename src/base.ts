c66284ed7c080a66f2c35ce228b25dd115e4694a
qwq <qwq@qwq.cat>
2024-01-13 10:27:59 +0800

    feat: ignore `htmlWhitespaceSensitivity` & add more defaults

--- src/base.ts
+++ src/base.ts
@@ -1,10 +1,22 @@
 import { defineConfig } from './utils';
 
+/** @see https://prettier.io/docs/en/options */
 export default defineConfig({
+  arrowParens: 'always',
+  bracketSameLine: false,
+  bracketSpacing: true,
+  embeddedLanguageFormatting: 'auto',
   endOfLine: 'lf',
+  htmlWhitespaceSensitivity: 'ignore',
+  jsxSingleQuote: false,
   printWidth: 120,
+  proseWrap: 'preserve',
+  quoteProps: 'as-needed',
   semi: true,
+  singleAttributePerLine: false,
   singleQuote: true,
   tabWidth: 2,
+  trailingComma: 'all',
   useTabs: false,
+  vueIndentScriptAndStyle: false,
 } as const);
